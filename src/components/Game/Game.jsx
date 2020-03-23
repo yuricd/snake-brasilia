7e7cef04cf9af05b67a4dbecc38f75dac6651243
Yuri Delgado <yuridelgado@localhost.localdomain>
2020-03-23 00:29:14 -0300

    Hook very slow

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -4,46 +4,20 @@ import { useSnake } from '../../hooks/snake';
 
 function Game({ setScore }) {
 
-  const canvasRef = React.useRef(null);
+  const canvas = React.useRef(null);
 
-  const [showInfoBox, setShowInfoBox] = React.useState(false);
+  const [showInfoBox,] = React.useState(false);
 
-  const eatCallback = React.useCallback((newScore) => {
-    setScore(newScore);
-    setShowInfoBox(true);
-  }, [setScore]);
+  // const eatCallback = React.useCallback((newScore) => {
+  //   setScore(newScore);
+  //   setShowInfoBox(true);
+  // }, [setScore]);
 
-  const snakeHook = useSnake(canvasRef, eatCallback);
-
-  const getDirection = React.useCallback((e) => {
-    if (e.keyCode === 37 && snakeHook.direction !== 'right') {
-      snakeHook.setState({ direction: 'left' });
-    } else if (e.keyCode === 38 && snakeHook.direction !== 'down') {
-      snakeHook.setState({ direction: 'up' });
-    } else if (e.keyCode === 39 && snakeHook.direction !== 'left') {
-      snakeHook.setState({ direction: 'right' });
-    } else if (e.keyCode === 40 && snakeHook.direction !== 'up') {
-      snakeHook.setState({ direction: 'up' });
-    }
-  }, [snakeHook]);
-
-  const callback = React.useCallback(() => {
-    snakeHook.draw();
-  }, [snakeHook]);
+  const snakeHook = useSnake({ canvas });
   
-  React.useEffect(() => {
-    document.addEventListener('keydown', getDirection);
-  
-    setInterval(() => callback(), 70);
-  }, []);
-
-  React.useEffect(() => {
-    snakeHook.setState({ canvas: canvasRef.current })
-  }, [canvasRef])
-
   return (
     <section className={styles.main}>
-      <canvas className={styles.canvas} ref={canvasRef} width="600" height="600"></canvas>
+      <canvas className={styles.canvas} ref={canvas} width="600" height="600"></canvas>
 
       {showInfoBox && (
         <div>
--- src/hooks/snake.js
+++ src/hooks/snake.js
@@ -1,131 +1,219 @@
-import React from 'react';
-import snakeRight from '../assets/images/snake-head-right.png';
-import snakeDown from '../assets/images/snake-head-down.png';
-import snakeLeft from '../assets/images/snake-head-left.png';
-import snakeUp from '../assets/images/snake-head-up.png';
-import { data } from '../data/politicians';
+import React, { useEffect, useLayoutEffect, useCallback } from "react";
+import snakeRight from "../assets/images/snake-head-right.png";
+import snakeDown from "../assets/images/snake-head-down.png";
+import snakeLeft from "../assets/images/snake-head-left.png";
+import snakeUp from "../assets/images/snake-head-up.png";
+import { data } from "../data/politicians";
 
 function reducer(res, newElement) {
   return {
     ...res,
-    ...newElement,
-  }
+    ...newElement
+  };
 }
 
-export const useSnake = ({ canvas, eatCallback }) => {
+export const useSnake = ({ canvas }) => {
+  let cpBody = [];
+  for (let i = 19; i >= 0; i--) {
+    cpBody.push({
+      x: i,
+      y: 10
+    });
+  }
 
   const init = {
-    canvas: null,
+    body: cpBody,
+    direction: "right",
     score: 20,
-    width: canvas?.width,
-    height: canvas?.height,
-    ctx: canvas?.getContext('2d'),
     snakeW: 7,
     snakeH: 7,
-    direction: 'right',
-    body: [],
     polits: data,
-    current: {},
-    eatCallback,
     paused: false,
   };
 
   const [state, setState] = React.useReducer(reducer, init);
 
-  console.log(state, data);
+  const getDirection = useCallback(
+    e => {
+      if (e.keyCode === 37 && state.direction !== "right") {
+        setState({ direction: "left" });
+        console.log("resetou1");
+      } else if (e.keyCode === 38 && state.direction !== "down") {
+        console.log("resetou2");
+        setState({ direction: "up" });
+      } else if (e.keyCode === 39 && state.direction !== "left") {
+        console.log("resetou3");
+        setState({ direction: "right" });
+      } else if (e.keyCode === 40 && state.direction !== "up") {
+        console.log("resetou4");
+        setState({ direction: "down" });
+      }
+    },
+    [state.direction]
+  );
+
+  useEffect(() => {
+    window.addEventListener("keydown", getDirection);
+    return () => {
+      window.removeEventListener("keydown", getDirection);
+    };
+  }, [getDirection]);
+
+  useLayoutEffect(() => {
+    const interval = setInterval(() => {
+      if (canvas && canvas.current) {
+        setState({
+          width: canvas.current.width,
+          height: canvas.current.height,
+          ctx: canvas.current.getContext("2d"),
+          current: getPolit(),
+        });
+
+        draw(ctx, width, height);
+      }
+    }, 70);
+
+    return () => clearInterval(interval);
+  });
 
   function drawSnake(x, y, head) {
     let headImg = new Image();
 
-    if (head && state.direction === 'right') {
+    if (head && state.direction === "right") {
+      console.log("1");
       headImg.src = snakeRight;
-      state.ctx.drawImage(headImg, x*state.snakeW-2, y*state.snakeH-8, 34, 18);
-    } else if (head && state.direction === 'left') {
+      state.ctx.drawImage(
+        headImg,
+        x * state.snakeW - 2,
+        y * state.snakeH - 8,
+        34,
+        18
+      );
+    } else if (head && state.direction === "left") {
+      console.log("2");
+
       headImg.src = snakeLeft;
-      state.ctx.drawImage(headImg, x*state.snakeW-24, y*state.snakeH-8, 34, 18);
-    } else if (head && state.direction === 'up') {
+      state.ctx.drawImage(
+        headImg,
+        x * state.snakeW - 24,
+        y * state.snakeH - 8,
+        34,
+        18
+      );
+    } else if (head && state.direction === "up") {
+      console.log("3");
+
       headImg.src = snakeUp;
-      state.ctx.drawImage(headImg, x*state.snakeW-8, y*state.snakeH-24, 18, 34);
-    } else if (head && state.direction === 'down') {
+      state.ctx.drawImage(
+        headImg,
+        x * state.snakeW - 8,
+        y * state.snakeH - 24,
+        18,
+        34
+      );
+    } else if (head && state.direction === "down") {
+      console.log("4");
+
       headImg.src = snakeDown;
-      state.ctx.drawImage(headImg, x*state.snakeW-3, y*state.snakeH-2, 18, 34);
+      state.ctx.drawImage(
+        headImg,
+        x * state.snakeW - 3,
+        y * state.snakeH - 2,
+        18,
+        34
+      );
     } else {
-      state.ctx.fillStyle = '#FFB800';
-      state.ctx.fillRect(x*state.snakeW, y*state.snakeH, state.snakeW, state.snakeH);
+      state.ctx.fillStyle = "#FFB800";
+      state.ctx.fillRect(
+        x * state.snakeW,
+        y * state.snakeH,
+        state.snakeW,
+        state.snakeH
+      );
     }
   }
 
   function checkCollision(x, y, array) {
-    if (x < 0 || 
-        y < 0 || 
-        x >= (state.width)/state.snakeW || 
-        y >= (state.height)/state.snakeH
-      ) {
-        alert('colidiu');
+    if (
+      x < 0 ||
+      y < 0 ||
+      x >= state.width / state.snakeW ||
+      y >= state.height / state.snakeH
+    ) {
+      alert("colidiu");
     }
-    for (let i=1; i<array.length; i++) {
-      if(x === array[i].x && y === array[i].y) {
-        alert('colidiu');
-      }
-    }
+    // for (let i=1; i<array.length; i++) {
+    //   if(x === array[i].x && y === array[i].y) {
+    //     alert('colidiu2');
+    //   }
+    // }
     return false;
   }
 
   function getPolit() {
     return {
-      data: state.polits[0],
+      data: state.polits ? state.polits[0] : data[0],
       pos: {
-        x: Math.round(Math.random()*(state.width/state.snakeW)),
-        y: Math.round(Math.random()*(state.height/state.snakeH)),
+        x: Math.round(Math.random() * (state.width / state.snakeW)),
+        y: Math.round(Math.random() * (state.height / state.snakeH))
       }
-    }
+    };
   }
 
   function drawPolit(renderImg, x, y) {
     state.ctx.shadowOffsetX = 0;
     state.ctx.shadowOffsetY = 0;
     state.ctx.shadowBlur = 1;
-    state.ctx.shadowColor = 'rgba(255, 255, 255, 1)';
-    state.ctx.drawImage(renderImg, x*state.snakeW-4, y*state.snakeH-4, state.snakeW+8, state.snakeH+8);
-    state.ctx.shadowColor = 'rgba(255, 255, 255, 0)';
+    state.ctx.shadowColor = "rgba(255, 255, 255, 1)";
+    state.ctx.drawImage(
+      renderImg,
+      x * state.snakeW - 4,
+      y * state.snakeH - 4,
+      state.snakeW + 8,
+      state.snakeH + 8
+    );
+    state.ctx.shadowColor = "rgba(255, 255, 255, 0)";
   }
 
   function draw() {
-    if(!state.current.data) {
-      console.log('end');
-    } else if (state.paused) { 
-      console.log('paused');
+    if (!state.current?.data) {
+      console.log("end");
+    } else if (state.paused) {
+      console.log("paused");
     } else {
       state.ctx.clearRect(0, 0, state.width, state.height);
 
-      for (let i=0; i<state.body.length; i++) {
+      for (let i = 0; i < state.body.length; i++) {
         let x = state.body[i].x;
         let y = state.body[i].y;
-        state.drawSnake(x, y, i === 0);
+        drawSnake(x, y, i === 0);
       }
 
       let renderImg = new Image();
       renderImg.src = state.current.data.image;
-      state.drawPolit(renderImg, state.current.pos.x, state.current.pos.y);
+      drawPolit(renderImg, state.current.pos.x, state.current.pos.y);
 
       let snakeHeadX = state.body[0].x;
       let snakeHeadY = state.body[0].y;
-      
-      state.checkCollision(snakeHeadX, snakeHeadY, state.body);
 
-      if(state.direction === 'left') {
+      checkCollision(snakeHeadX, snakeHeadY, state.body);
+
+      if (state.direction === "left") {
         snakeHeadX--;
-      } else if(state.direction === 'up') {
+      } else if (state.direction === "up") {
         snakeHeadY--;
-      } else if(state.direction === 'right') {
+      } else if (state.direction === "right") {
         snakeHeadX++;
-      } else if(state.direction === 'down') {
+      } else if (state.direction === "down") {
         snakeHeadY++;
       }
 
-      if (snakeHeadX === state.current.pos.x && snakeHeadY === state.current.pos.y) {
+      if (
+        snakeHeadX === state.current.pos.x &&
+        snakeHeadY === state.current.pos.y
+      ) {
         state.polits.shift();
-        state.current = state.getPolit();
+        state.current = getPolit();
         state.setScore(state.current.data.salary);
         state.eatCallback(state.score);
         state.paused = true;
@@ -135,19 +223,19 @@ export const useSnake = ({ canvas, eatCallback }) => {
 
       let newHead = {
         x: snakeHeadX,
-        y: snakeHeadY,
-      }
+        y: snakeHeadY
+      };
 
-      state.body.unshift(newHead);
+      setState({ body: [newHead].concat(state.body) });
     }
   }
-  
+
   return {
     getPolit,
     setState,
     drawSnake,
     checkCollision,
     drawPolit,
-    draw,
-  }
-}
+    draw
+  };
+};
